fa62d8d18584a01c2c9527c75fbe5e2443c1be67
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-02-12 23:58:48 -0800

    added files

--- tutorial/11-methods.js
+++ tutorial/11-methods.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const app = express();
 let { people } = require("./data");
+
+
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
